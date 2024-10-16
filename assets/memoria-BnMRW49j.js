0c9d2b78adfaefec7c9960d189882f6a4fdbd813
alibhtty <77213825+alibhtty@users.noreply.github.com>
2024-10-16 05:14:33 +0200

    1.0.1

---
+++ assets/memoria-BnMRW49j.js
@@ -0,0 +1,2 @@
+import{j as e}from"./index-SEQU3KHB.js";const i=()=>e.jsx("div",{className:"game",children:e.jsx("iframe",{src:"./memoria/index.html",title:"Memoria",width:"100%",height:"100%",frameBorder:"0"})});export{i as default};
+//# sourceMappingURL=memoria-BnMRW49j.js.map
