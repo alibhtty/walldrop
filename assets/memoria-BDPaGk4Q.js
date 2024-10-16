07d087215bb7d590230e9b95eda86db1931819ff
alibhtty <77213825+alibhtty@users.noreply.github.com>
2024-10-16 17:05:20 +0200

    1.0.2

--- assets/memoria-BDPaGk4Q.js
+++
@@ -1,2 +0,0 @@
-import{j as e}from"./index-C35UnOb7.js";const i=()=>e.jsx("div",{className:"game",children:e.jsx("iframe",{src:"./memoria/index.html",title:"Memoria",width:"100%",height:"100%",frameBorder:"0"})});export{i as default};
-//# sourceMappingURL=memoria-BDPaGk4Q.js.map
